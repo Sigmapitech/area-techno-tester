d702b992c88d1732d209446dc6a5302cad97a2d6
Sigmanificient <edhyjox@gmail.com>
2025-09-20 23:42:55 +0200

    feat(db/mongo): Add mongo scripts

--- considerations/db-mongo/flake.nix
+++ considerations/db-mongo/flake.nix
@@ -1,9 +1,20 @@
 {
   outputs = { self }: let
-    pkgs = import ../../nixpkgs.nix { system = "x86_64-linux"; };
+    pkgs = import ../../nixpkgs.nix {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
   in {
-    devShells.${pkgs.system}.default = pkgs.mkShell {
-      packages = [ ];
+    devShells.${pkgs.system}.default = let
+      pyenv = pkgs.python3.withPackages (p: [
+        p.pymongo
+        p.graphviz
+      ]);
+    in pkgs.mkShell {
+      packages = [
+        pyenv
+        pkgs.mongodb
+      ];
     };
   };
 }
