e4a3b95201dc36b391b8921f0ebf3e930c405434
Sigmanificient <edhyjox@gmail.com>
2025-09-20 22:15:23 +0200

    feat(con/sqlite): Add schema & populate script

---
+++ considerations/db-sqlite/flake.nix
@@ -0,0 +1,17 @@
+{
+  outputs = { self }: let
+    pkgs = import ../../nixpkgs.nix { system = "x86_64-linux"; };
+  in {
+    devShells.${pkgs.system}.default = let
+      pyenv = pkgs.python3.withPackages (p: [
+        p.graphviz
+      ]);
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        graphviz
+        pyenv
+        sqlite
+      ];
+    };
+  };
+}
