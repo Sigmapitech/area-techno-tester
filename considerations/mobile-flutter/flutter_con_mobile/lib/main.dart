bac30d88383e77c0a725d4d6d6f334555163c76c
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-09-20 22:11:09 +0200

    feat(consideration): add button for flutter tutorial

--- considerations/mobile-flutter/flutter_con_mobile/lib/main.dart
+++ considerations/mobile-flutter/flutter_con_mobile/lib/main.dart
@@ -26,6 +26,12 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
@@ -35,7 +41,14 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () { appState.getNext(); },
+            child: Text('Next'),
+          ),
+        ],
       ),
     );
   }
