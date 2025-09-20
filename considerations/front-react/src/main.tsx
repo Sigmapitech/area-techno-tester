11ce98564f8d032d6cd7c6ce8ea3bf32c59c5174
Sigmanificient <edhyjox@gmail.com>
2025-09-20 19:44:32 +0200

    feat(con/react): Add react-flow

--- considerations/front-react/src/main.tsx
+++ considerations/front-react/src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.scss'
 
 import HomePage from './routes/home';
+import GraphPage from './routes/graph';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/graph" element={<GraphPage />} />
     </Routes>
   </BrowserRouter>,
 )
