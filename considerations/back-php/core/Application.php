715a2543bb9ec221884c18d256aa25b35b264cdc
Sigmanificient <edhyjox@gmail.com>
2025-09-20 20:38:57 +0200

    feat(con/php): create the route & dummy endpoint

--- considerations/back-php/core/Application.php
+++ considerations/back-php/core/Application.php
@@ -11,19 +11,60 @@ class Application extends Dispatcher
         echo '</pre>';
     }
 
+    public static function _notFound() {
+        Response::status(404);
+        Response::json(['error' => 'Not found']);
+    }
+
+    public static function _forbidden() {
+        Response::status(403);
+        Response::json(['error' => 'Forbidden']);
+    }
+
     public static function dispatch() {
-        $path = Request::getPath()[0];
+        $params = Request::getPath();
 
-        if (isset($_SESSION['init'])) {
-          echo '<script>localStorage.removeItem("startDate");</script>';
-          unset($_SESSION['init']);
-        }
-
-        if ($path === 'debug') {
+        if ($params[0] === 'debug') {
             self::_debug();
             return;
         }
 
-        Response::put('index.html');
+        array_shift($params);
+
+        $rawEndpoint = array_shift($params);
+
+        if ($rawEndpoint === null || $rawEndpoint === '') {
+            readfile('index.html'); 
+            return;
+        }
+
+        $endpointName = ucfirst($rawEndpoint);
+        $endpointPath = 'api/' . $endpointName . '.php';
+
+        if (!file_exists($endpointPath)) {
+            self::_notFound();
+            return;
+        }
+
+        require_once $endpointPath;
+        $endpoint = new $endpointName();
+
+        $method = Request::getMethod();
+
+        $func_name = array_shift($params);
+        if ($func_name === null) {
+            self::_notFound();
+            return;
+        }
+
+        $action = $method . ucfirst($func_name);
+
+        if (!method_exists($endpoint, $action)) {
+            self::_notFound();
+            return;
+        }
+
+        Response::status(200);
+        Response::json($endpoint->$action() ?? []);
     }
 }
--- considerations/back-php/run.sh
+++ considerations/back-php/run.sh
@@ -1,1 +1,1 @@
-php -S localhost:8000 -t .
+php -S localhost:8000 index.php
