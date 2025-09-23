fabc7897793fc004c5cb8607537e478bd885b657
Sigmanificient <edhyjox@gmail.com>
2025-09-23 02:48:59 +0200

    feat(pocs/react): Login page

--- pocs/front-react/src/main.tsx
+++ pocs/front-react/src/main.tsx
@@ -1,16 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import './index.scss'
+import "./index.scss";
 
-import HomePage from './routes/home';
-import GraphPage from './routes/graph';
+import GraphPage from "./routes/graph";
+import HomePage from "./routes/home";
+import LoginPage from "./routes/login";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/graph" element={<GraphPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   </BrowserRouter>,
-)
+);
